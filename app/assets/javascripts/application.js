6f96fecf0b9ae8510432d3df232ab1abe80c22d5
Douglas Teixeira <teixeir3@gmail.com>
2015-09-14 14:49:33 -0400

    Adds bug fix for application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,17 +1,16 @@
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap
+//= require turbolinks
+
+//= require bootstrap-sprockets
 //= require animate-scroll
 //= require fileinput
 
 //= require underscore
 //= require backbone
 //= require lamington_properties
-//= require_tree ../templates
-//= require_tree ./models
-//= require_tree ./collections
-//= require_tree ./views
-//= require_tree ./routers
+
+
 
 
 var setFilepickerKey = function() {
